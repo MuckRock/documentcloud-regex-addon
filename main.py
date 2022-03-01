25cf1f69fc43f7b770571a92438a28d7a68c7c9d
Mitchell Kotler <mitch@mitchellkotler.net>
2022-02-28 21:14:44 -0500

    regex add-on

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This is an add-on to search a document for a regex and output all of the matches
 """
+
+import json
+import re
 
 from addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class Regex(AddOn):
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
+        regex = self.data["regex"]
+        pattern = re.compile(regex)
 
-        self.set_message("Hello World start!")
+        matches = {}
 
-        # add a hello note to the first page of each selected document
-        if self.documents:
-            length = len(self.documents)
-            for i, doc_id in enumerate(self.documents):
-                self.set_progress(100 * i // length)
-                document = self.client.documents.get(doc_id)
-                document.annotations.create(f"Hello {name}!", 0)
-        elif self.query:
-            documents = self.client.documents.search(self.query)
-            length = len(documents)
-            for i, document in enumerate(documents):
-                self.set_progress(100 * i // length)
-                document.annotations.create(f"Hello {name}!", 0)
+        for doc_id in self.documents:
+            document = self.client.documents.get(doc_id)
+            matches[doc_id] = pattern.findall(document.full_text)
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
+        with open("matches.json", "w+") as file_:
+            file_.write(json.dumps(matches, indent=4))
             self.upload_file(file_)
 
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
-
 if __name__ == "__main__":
-    HelloWorld().main()
+    Regex().main()

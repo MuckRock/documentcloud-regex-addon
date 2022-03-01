36f5e10654ca75d6ee2f7276e707abc1fe304713
Mitchell Kotler <mitch@mitchellkotler.net>
2022-03-01 16:09:11 -0500

    switch from json to csv

--- main.py
+++ main.py
@@ -2,27 +2,29 @@
 This is an add-on to search a document for a regex and output all of the matches
 """
 
-import json
+import csv
 import re
 
 from addon import AddOn
 
 
 class Regex(AddOn):
-
     def main(self):
         regex = self.data["regex"]
         pattern = re.compile(regex)
 
-        matches = {}
+        with open("matches.csv", "w+") as file_:
+            writer = csv.writer(file_)
 
-        for doc_id in self.documents:
-            document = self.client.documents.get(doc_id)
-            matches[doc_id] = pattern.findall(document.full_text)
+            for doc_id in self.documents:
+                document = self.client.documents.get(doc_id)
+                writer.writerows(
+                    [m, document.canonical_url]
+                    for m in pattern.findall(document.full_text)
+                )
 
-        with open("matches.json", "w+") as file_:
-            file_.write(json.dumps(matches, indent=4))
             self.upload_file(file_)
+
 
 if __name__ == "__main__":
     Regex().main()

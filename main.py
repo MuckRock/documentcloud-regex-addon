edb6f42fa87782667427337156bf95ec6a1dc2a3
Mitchell Kotler <mitch@mitchellkotler.net>
2022-11-15 13:45:21 -0500

    proof of concept for using position information

--- main.py
+++ main.py
@@ -22,6 +22,31 @@ class Regex(AddOn):
             writer.writerow(["match", "url"])
 
             for document in self.client.documents.list(id__in=self.documents):
+
+                if self.data.get("annotate"):
+                    # Example code: I am only looking at the first page
+                    # This should loop through all pages
+                    url = (
+                        document.asset_url + f"documents/{document.id}/pages/"
+                        f"{document.slug}-p1.position.json"
+                    )
+                    # Should error check this
+                    resp = self.client.get(url, full_url=True)
+                    positions = resp.json()
+                    print(positions[:3])
+                    for info in positions:
+                        # This will only match the regular expression against
+                        # one word at a time - this can be altered as needed
+                        if pattern.search(info["text"]):
+                            document.annotations.create(
+                                f"{regex} Match",
+                                0,
+                                x1=info["x1"],
+                                y1=info["y1"],
+                                x2=info["x2"],
+                                y2=info["y2"],
+                            )
+
                 writer.writerows(
                     [m, document.canonical_url]
                     for m in pattern.findall(document.full_text)
